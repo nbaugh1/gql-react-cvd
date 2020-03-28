e9d91ac4200e29faa813a5ddaa75366606f271a2
nbaugh1 <nbaugh1@gmail.com>
2020-03-28 18:00:39 -0400

    heroku

---
+++ public/build/precache-manifest.be9f8d83b6d136ec5b55af3e3f9e3e76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d14812ce96d039a2258fba630ddef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8c93a4fd594bbead15",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "99206f254e35a6bbf895",
+    "url": "/static/js/2.510ed7ec.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/static/js/2.510ed7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8c93a4fd594bbead15",
+    "url": "/static/js/main.36ca1e8e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "f76c559880a41738927f4815439e421a",
+    "url": "/static/media/logo.f76c5598.jpg"
+  }
+]);
