e43b0c1fd310c8c75c75d7e6428f8e004b758e1e
nbaugh1 <nbaugh1@gmail.com>
2020-03-28 00:00:30 -0400

    add logo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
+import logo from './logo.jpg'
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
@@ -13,6 +15,11 @@ function App () {
     <ApolloProvider client={client}>
     <Router>
       <div className='container'>
+      <img
+          src={logo}
+          alt='SpaceX'
+          style={{ height:200, margin: 'auto', display: 'block' }}
+        />
        <h1>Home</h1>
       </div>
       </Router>
