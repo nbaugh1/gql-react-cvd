9f85ff21cf2c21c8b70ee972a444b68777d1e365
nbaugh1 <nbaugh1@gmail.com>
2020-03-28 22:34:21 -0400

    refactor client to use apollo client and useQueryhook

--- client/src/components/States.js
+++ client/src/components/States.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
+import { useQuery } from 'react-apollo'
+import StateItem from './StateItem'
 // import StateItem from './LaunchItem'
 
 const STATES_QUERY = gql`
@@ -15,28 +16,17 @@ const STATES_QUERY = gql`
 	}
 }
 `
-export default class StatesList extends Component {
-    render() {
-        console.log(STATES_QUERY)
-        return (
-            <div>
-                <Query query={STATES_QUERY}>
-                    {({ loading, error, data }) => {
-                        if (loading) return <h4>Loading...</h4>
-                        if (error) console.log(error)
-                        console.log(data)
-                        return <Fragment>
-                            {
-                                data.states.map(state => (
-                                   <div><p>{state.state}: {state.cases} Cases, {state.deaths} Deaths, {state.todayCases} New cases today, {state.todayDeaths} Deaths today</p></div> 
-                                ))
-                            }
-                        </Fragment>
-                    }}
-                </Query>
-            </div>
-        )
-    }
+const StatesList= () => {
+    const { data, loading, error } = useQuery(STATES_QUERY)
+    if (loading) return <h4>Loading...</h4>
+    if (error) console.log(error)
+    return (
+        data.states.map(state => (
+            <StateItem key={state.state} state={state} />
+        ))
+    )
 }
 
 
+
+export default StatesList
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
