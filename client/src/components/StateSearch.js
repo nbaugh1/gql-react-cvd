1d9e880d844e7aff320ebdff07b75ac15e50a81d
nbaugh1 <nbaugh1@gmail.com>
2020-04-07 15:49:39 -0400

    add search result component under search field

--- client/src/components/StateSearch.js
+++ client/src/components/StateSearch.js
@@ -1,4 +1,4 @@
-import  React, { useState, useEffect }  from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import StateItem from './StateItem'
@@ -6,40 +6,48 @@ import Dump from '../components/Dump'
 
 const StateSearch = data => {
     // debugger
-    const [searchTerm, setSearchTerm] = useState("")
+    const [searchTerm, setSearchTerm] = useState(null)
     const [searchResults, setSearchResults] = useState([]);
     const handleChange = event => {
         setSearchTerm(event.target.value)
     }
-    
+
     useEffect(() => {
         const results = data.states.states.filter(state =>
-                state['state'].toLowerCase().includes(searchTerm)
-            );
+            state['state'].toLowerCase().includes(searchTerm)
+        );
         setSearchResults(results)
     }, [searchTerm]);
-    
-    const handleSubmit = event => {
-        event.preventDefault()
-        console.log(searchResults)
-    }
-    
+
+    // const handleSubmit = event => {
+    //     const resultComponent = <StateItem state={searchResults[0]} />
+    //     event.preventDefault()
+    //     console.log(searchResults)
+    //     debugger
+    // }
+
     return (
-        <div style={{margin: '10px'}}>
-            <Form onSubmit={handleSubmit}>
+        <div style={{ margin: '10px' }}>
+            <Form >
                 <Form.Group controlId='stateSearchForm'>
                     <Form.Label>Search</Form.Label>
-                    <Form.Control 
-                        type='text' 
+                    <Form.Control
+                        type='text'
                         onChange={handleChange}
                         value={searchTerm}
-                       
+
                     />
-                    <Button variant='secondary' type="submit" value="Search" />
+                    {/* <Button variant='secondary' type="submit" value="Search" /> */}
                 </Form.Group>
-                
+
             </Form>
-           <Dump data={searchResults} />
+            <div>
+                <ul>
+                    {searchResults.map(state => (
+                    <li><StateItem state={state} /></li>))}
+                </ul>
+            </div>
+
         </div>
     )
 }
